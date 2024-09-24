c4109114909242b600154beb2b09a84163a0bfb2
EarlyOcean <86952523+EarlyOcean@users.noreply.github.com>
2024-09-24 14:34:34 +0700

    Update Test1.cpp

--- Pertemuan 5/Soal 1/Test1.cpp
+++ Pertemuan 5/Soal 1/Test1.cpp
@@ -2,5 +2,5 @@
 using namespace std;
 
 int main(){
-    cout << "Wait, this problem need test cases??";
+    cout << "Wait, this problem need test cases? (Do make run to print 1-100 in fizzbuzz instead)";
 }
